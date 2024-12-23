d95aeaaf488202b8e6b12005d0e7e8cfe6dadf18
Mohammed kazim Shaikh <116196187+Moon-kazim@users.noreply.github.com>
2024-12-23 10:36:46 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,16 @@
+# Write your code below this line 👇
+print("hello World!")
+print("")
+# 1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+# 2. Knead the dough for 10 minutes.
+# 3. Add 3g of Salt.
+# 4. Leave to rise for 2 hours.
+# 5. Bake at 200 degrees C for 30 minutes.
+print("Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("Knead the dough for 10 minutes.")
+print("Add 3g of Salt.")
+print("Leave to rise for 2 hours.")
+print("Bake at 200 degrees C for 30 minutes.")
+print("")
+print("Hello world!\nhello World!\nHello World!\n")
+print("Hello"+" "+"kazim")
